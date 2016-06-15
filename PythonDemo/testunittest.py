e6850a6069257424358f6c0db0f659937f2ae8eb
jinhong666 <jinhong618@126.com>
2016-06-15 17:49:16 +0800

    subtest
    subtest

--- PythonDemo/testunittest.py
+++ PythonDemo/testunittest.py
@@ -5,6 +5,15 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
 
+    def test_even(self):
+        '''
+        Test that numbers between 0 and 5 ara all even
+        :return:
+        '''
+        for i in range(0,6):
+            with self.subTest(i=i):
+                self.assertEqual(i % 2 ,0)
+
 
 if __name__ == '__main__':
     unittest.main()
